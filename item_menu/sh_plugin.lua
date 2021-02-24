f34f53dd78188ff8c1208140ddabba9792d268ce
ZeMysticalTaco <zemysticaltaco@gmail.com>
2021-02-24 12:55:32 -0600

    Cleanup item_menu/sh_plugin.lua

--- item_menu/sh_plugin.lua
+++ item_menu/sh_plugin.lua
@@ -2,21 +2,18 @@ PLUGIN.name = 'Admin Item Menu'
 PLUGIN.description = 'Adds an item menu for admins.'
 PLUGIN.author = 'TomSL / ZeMysticalTaco'
 
-ix.util.Include('cl_plugin.lua')
-ix.util.Include('sv_plugin.lua')
+ix.util.Include( 'cl_plugin.lua' )
+ix.util.Include( 'sv_plugin.lua' )
+ix.util.Include( 'cl_panels.lua' )
 
-if (!CAMI.GetPrivilege("Helix - Item Menu")) then
-    CAMI.RegisterPrivilege({
+if ( not CAMI.GetPrivilege( "Helix - Item Menu" ) ) then
+    CAMI.RegisterPrivilege( {
         Name = 'Helix - Item Menu',
         MinAccess = "superadmin",
         Description = 'Whether or not this user has access to the Context Menu Item Spawner.'
-    })
+    } )
 end
 
-function PLUGIN:CanPlayerUseItemMenu(client)
-	return CAMI.PlayerHasAccess(client, "Helix - Item Menu", nil) or client:IsSuperAdmin()
+function PLUGIN:CanPlayerUseItemMenu( client )
+    return CAMI.PlayerHasAccess( client, "Helix - Item Menu", nil ) or client:IsSuperAdmin( )
 end
-
-ix.util.Include('cl_panels.lua')
-
---TODO RIGHT CLICK TO GIVE TO SELF???
