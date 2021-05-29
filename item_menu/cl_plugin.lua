3b26f8d8346790cef3fa37d2fd86765c2302b32f
Wolffe <Wolffebusiness@outlook.com>
2021-05-29 10:03:31 -0400

    Hotfix sudden issue where button did not yeet itself properly OnContextMenuClose

--- item_menu/cl_plugin.lua
+++ item_menu/cl_plugin.lua
@@ -6,15 +6,21 @@ function PLUGIN:OnContextMenuOpen( )
 end
 
 function PLUGIN:OnContextMenuClose( )
-	if ( IsValid( ix.gui.itemmenutoggle ) and IsValid(ix.gui.itemmenu)) then
+	if ( IsValid( ix.gui.itemmenutoggle )) then
 		ix.gui.itemmenutoggle:MoveTo( ScrW( ) + 350, 0, 0.2, 0, -1, function( )
 			ix.gui.itemmenutoggle:SetVisible( false )
 		end )
+
+		--for live debugging, uncommenting these will rebuild the panel each time you close the context menu, please don't enable this live!
+		--ix.gui.itemmenu:Remove( )
+		--vgui.Create( "ixItemMenuToggle" )
+	end
+	if (IsValid(ix.gui.itemmenu)) then
 		ix.gui.itemmenu:MoveTo( ScrW( ) + 350, 0, 0.2, 0, -1, function( )
 			ix.gui.itemmenu:SetVisible( false )
 		end )
 
-		--for live debugging, uncommenting these will rebuild the panel each time you close the context menu, please don't enable this live.
+		--for live debugging, uncommenting these will rebuild the panel each time you close the context menu, please don't enable this live!
 		--ix.gui.itemmenu:Remove( )
 		--vgui.Create( "ixItemMenu" )
 	end
